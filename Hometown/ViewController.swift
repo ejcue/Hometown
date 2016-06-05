89c1aae62cb7a661ce4a51309480d6cfab0527f9
Eric Cuevas <ejcue@me.com>
2016-06-05 16:15:00 -0500

    added files

--- Hometown/ViewController.swift
+++ Hometown/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moreBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        moreBtn.layer.cornerRadius = 2.0
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
